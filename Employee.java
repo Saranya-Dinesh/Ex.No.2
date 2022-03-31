597a436ac63ac5dbc13b05f121b237d7c0a3aef3
Saranya-Dinesh <102136479+Saranya-Dinesh@users.noreply.github.com>
2022-03-31 14:25:33 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+package org.kiot.employee;
+
+import java.util.Scanner;
+import org.kiot.employeeoperation.EmployeeOpertaion;
+
+public class Employee {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeOpertaion employeeOpertaion = new EmployeeOpertaion();
+		employeeOpertaion.doEmployeeOpertaion();
+	}
+}
